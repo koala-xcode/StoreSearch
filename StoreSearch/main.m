e721fc452a9bdb8588a51079ad9e2d556b7c23e4
Myo <mkyaw24@googlemail.com>
2012-04-29 15:12:22 +0800

    Initial Commit

---
+++ StoreSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoreSearch
+//
+//  Created by Koala on 4/29/12.
+//  Copyright (c) 2012 KNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
