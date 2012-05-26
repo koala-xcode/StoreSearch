f7a4224a20b4d15c2d41430efae427fcff9f2ec4
Myo <mkyaw24@googlemail.com>
2012-05-26 13:03:04 +0800

    name change to ViewController to SearchViewController

---
+++ StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Koala on 4/29/12.
+//  Copyright (c) 2012 KNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
